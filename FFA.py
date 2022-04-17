5a0a28e17357dde584c21d5e5f956da1f500b4f5
Kun Xing <103901422+KunXingX@users.noreply.github.com>
2022-04-17 16:08:15 -0400

    Update FFA.py

--- FFA.py
+++ FFA.py
@@ -8,7 +8,7 @@ class PALayer(nn.Module):
     def __init__(self, channel):
         super(PALayer, self).__init__()
         self.pa = nn.Sequential(
-                nn.Conv2d(channel, channel // 8, 1, padding=0, bias=True),
+                nn.Conv2d(channel, channel // 8, 3, padding=0, bias=True),
                 nn.ReLU(inplace=True),
                 nn.Conv2d(channel // 8, 1, 1, padding=0, bias=True),
                 nn.Sigmoid()
